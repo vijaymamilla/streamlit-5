c42f4d1dea256fdd97382bc58a3165b433b06fff
Vijay Mamilla <vijay.mamilla@gmail.com>
2023-12-15 10:04:58 -0500

    updated the models

--- app/pages/5-Khulna_Weather.py
+++ app/pages/5-Khulna_Weather.py
@@ -11,13 +11,30 @@ np.random.seed(42)
 tf.random.set_seed(42)
 tf.keras.utils.set_random_seed(42)
 
+st.header("**Flood Guard - Khulna Region**")
+
+st.write("""
+    **Model Overview:**
+    The study predicts the daily average precipitation for Khulna district using LSTM model.
+    The model’s performance was evaluated with the mean absolute error: 6.71. 
+    """)
+
+st.write("""
+    **Future Work:**
+    To improve, we can study the outliers and consider more hyperparameters for LSTM.
+    """)
+
+
 def header():
     st.header("Khulna Weather Forecasting")
 
+
 @st.cache_data
 def load_test_df():
-    test_df = pd.read_csv('app/artifactory/khulna_test.csv', index_col='datetime', parse_dates=True)
+    test_df = pd.read_csv('app/artifactory/khulna_test.csv',
+                          index_col='datetime', parse_dates=True)
     return test_df
+
 
 @st.cache_resource
 def build_lstm_model():
@@ -25,8 +42,10 @@ def build_lstm_model():
 
     return new_model
 
+
 def show_search_query():
-    query = st.number_input("Enter Number of days ",min_value=1,max_value=100,value=14,step=1)
+    query = st.number_input("Enter Number of days ",
+                            min_value=1, max_value=100, value=14, step=1)
 
     if query:
         days = query
@@ -36,29 +55,35 @@ def show_search_query():
         st.write(str(days)+" days weather forecast")
         st.table(new_df)
 
+
 class DataWindow:
     def __init__(self, input_width, label_width, shift, test_df, label_columns=None):
         self.test_df = test_df
         self.label_columns = label_columns
         if label_columns is not None:
-            self.label_columns_indices = {name: i for i, name in enumerate(label_columns)}
-        self.column_indices = {name: i for i, name in enumerate(test_df.columns)}
+            self.label_columns_indices = {
+                name: i for i, name in enumerate(label_columns)}
+        self.column_indices = {name: i for i,
+                               name in enumerate(test_df.columns)}
         self.input_width = input_width
         self.label_width = label_width
         self.shift = shift
         self.total_window_size = input_width + shift
         self.input_slice = slice(0, input_width)
-        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+        self.input_indices = np.arange(self.total_window_size)[
+            self.input_slice]
         self.label_start = self.total_window_size - self.label_width
         self.labels_slice = slice(self.label_start, None)
-        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+        self.label_indices = np.arange(self.total_window_size)[
+            self.labels_slice]
 
     def split_to_inputs_labels(self, features):
         inputs = features[:, self.input_slice, :]
         labels = features[:, self.labels_slice, :]
         if self.label_columns is not None:
             labels = tf.stack(
-                [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+                [labels[:, :, self.column_indices[name]]
+                    for name in self.label_columns],
                 axis=-1
             )
         inputs.set_shape([None, self.input_width, None])
@@ -101,7 +126,8 @@ def predict(days: int):
 
 def main():
     header()
-    predict(14) # model is compiled with 14 days, i am using default value as 14 days
+    # model is compiled with 14 days, i am using default value as 14 days
+    predict(14)
     show_search_query()
 
 
